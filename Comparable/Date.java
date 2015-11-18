fd4a336f628013d118b617bb9dc0b387b890ef38
yulongtan <swifties@uw.edu>
2015-11-18 13:45:16 -0800

    added date

---
+++ Comparable/Date.java
@@ -0,0 +1,9 @@
+public int compareTo(CalendarDate other) {
+    if (this.year != other.year) {
+        return ((Integer)this.year).compareTo(other.year);
+    } else if (this.month != other.month) {
+        return ((Integer)this.month).compareTo(other.month);
+    } else {
+        return ((Integer)this.day).compareTo(other.day);
+    }
+}
