376f8ce240f755ead88827461d474da2da85c851
yulongtan <swifties@uw.edu>
2015-11-19 11:55:04 -0800

    added is unique

---
+++ SetsMaps/isUnique.java
@@ -0,0 +1,11 @@
+public boolean isUnique(Map<String, String> map) {
+    Set<String> values = new HashSet<String>();
+    for (String value : map.values()) {
+        if (values.contains(value)) {
+            return false;
+        } else {
+            values.add(value);
+        }
+    }
+    return true;
+}
