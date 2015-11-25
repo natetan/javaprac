a24e44a784f3edf98084fcf77fe1729ca19716d8
Yulong Tan <swifties@uw.edu>
2015-11-25 11:51:25 -0800

    created a method to reverse key value pairs in a map

---
+++ SetsMaps/reverse.java
@@ -0,0 +1,7 @@
+public static Map<String, Integer> reverse(Map<Integer, String> m1) {
+    Map<String, Integer> reverse = new HashMap<String, Integer>();
+    for (int num : m1.keySet()) {
+        reverse.put(m1.get(num), num);
+    }
+    return reverse;
+}
