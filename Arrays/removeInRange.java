0fdb9ba47c03183014ea6c093e4642b185334b24
yulongtan <swifties@uw.edu>
2015-12-03 12:32:16 -0800

    wrote a method that removes all occurrences of a specified number in a given index of a list

---
+++ Arrays/removeInRange.java
@@ -0,0 +1,7 @@
+public static void removeInRange(ArrayList<Integer> list, int num, int start, int end) {
+    for (int i = end - 1; i >= start; i--) {
+        if (list.get(i) == num) {
+            list.remove(i);
+        }
+    }
+}
