98f7898cf709dc0ca53d7f508c08487a23702a9a
yulongtan <swifties@uw.edu>
2015-12-11 11:42:42 -0800

    wrote a method that moves the element from the front of a linked list to the back

---
+++ LinkedList/frontToBack.java
@@ -0,0 +1,11 @@
+public void frontToBack() {
+    if (front != null && front.next != null) {
+        ListNode current = this.front;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = this.front;
+        this.front = this.front.next;
+        current.next.next = null;
+    }
+}
