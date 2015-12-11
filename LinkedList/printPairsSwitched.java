fe9bd38460adef9f9cd311d6c2f36d835146d726
Yulong Tan <swifties@uw.edu>
2015-12-10 22:20:00 -0800

    wrote a method that prints swapped pairs from a linked list

---
+++ LinkedList/printPairsSwitched.java
@@ -0,0 +1,11 @@
+public void printPairsSwitched() {
+    ListNode current = this.front;
+    while (current != null && current.next != null) {
+        System.out.print(current.next.data + " " + current.data + " ");
+        current = current.next.next;
+    }
+    if (current != null && current.next == null) {
+        System.out.print(current.data);
+    }
+    System.out.println();
+}
