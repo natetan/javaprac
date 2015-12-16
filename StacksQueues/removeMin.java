57e95cdb5ebae072fd74c52373aad460d01fe3f3
Yulong Tan <swifties@uw.edu>
2015-12-15 19:28:18 -0800

    wrote a method that removes the min from a stack and returns it

---
+++ StacksQueues/removeMin.java
@@ -0,0 +1,23 @@
+public static int removeMin(Stack<Integer> s) {
+    Queue<Integer> q = new LinkedList<Integer>();
+    int min = Integer.MAX_VALUE;
+    while(!s.isEmpty()) {
+        int value = s.pop();
+        min = Math.min(value, min);
+        q.add(value);
+    }
+    
+    while (!q.isEmpty()) {
+        int value = q.remove();
+        if (value != min) {
+            s.push(value);
+        }
+    }
+    while (!s.isEmpty()) {
+        q.add(s.pop());
+    }
+    while (!q.isEmpty()) {
+        s.push(q.remove());
+    }
+    return min;
+}
