447b8312e779add4072d6f8a0fb871c776db813b
yulongtan <swifties@uw.edu>
2016-01-28 15:14:50 -0800

    updated it to use 1 loop

--- SetsMaps/maxOccurrences.java
+++ SetsMaps/maxOccurrences.java
@@ -1,15 +1,13 @@
 public static int maxOccurrences(List<Integer> list) {
     Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    int max = 0;
     for (int num : list) {
         if (map.containsKey(num)) {
             map.put(num, map.get(num) + 1);
         } else {
             map.put(num, 1);
         }
-    }
-    int max = 0;
-    for (int num : map.values()) {
-        max = Math.max(num, max);
+        max = Math.max(map.get(num), max);
     }
     return max;
 }
