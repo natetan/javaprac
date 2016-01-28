0eb5e13ff101daaa8c0701ae17db209499f672cf
yulongtan <swifties@uw.edu>
2016-01-28 15:45:52 -0800

    optimized solution

--- SetsMaps/split.java
+++ SetsMaps/split.java
@@ -1,11 +1,10 @@
 public static Map<Integer, Set<String>> split(Set<String> set) {
 	Map<Integer, Set<String>> map = new TreeMap<Integer, Set<String>>();
 	for (String s : set) {
-		if (map.containsKey(s.length())) {
-			map.get(s.length()).add(s);
-		} else {
+		if (!map.containsKey(s.length())) {
 			map.put(s.length(), new TreeSet<String>);
 		}
+		map.get(s.length()).add(s);
 	}
 	return map;
 }
