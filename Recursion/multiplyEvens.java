e96ba32c2d233a11883effb32f2ca3e86452ab2c
Yulong Tan <swifties@uw.edu>
2016-02-06 00:37:50 -0800

    multiplies first n even numbers

---
+++ Recursion/multiplyEvens.java
@@ -0,0 +1,28 @@
+public static int multiplyEvens(int n) {
+    if (n <= 0) {
+        throw new IllegalArgumentException();
+    }
+    if (n == 1) {
+        return 2;
+    } else {
+        return (n * 2) * multiplyEvens(n - 1);
+    }
+}
+
+/* pub priv method
+
+public static int multiplyEvens(int n) {
+    if (n <= 0) {
+        throw new IllegalArgumentException();
+    }
+    return multiplyEvens(n, n * 2);
+}
+
+private static int multiplyEvens(int n, int doubled) {
+    if (n == 1) {
+        return 2;
+    } else {
+        return doubled * multiplyEvens(n - 1, doubled - 2);
+    }
+}
+*/
