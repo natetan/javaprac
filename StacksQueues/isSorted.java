870c2e57784d3041abdbe6b3cfe681935d926d1d
yulongtan <swifties@uw.edu>
2016-02-10 10:41:49 -0800

    updated method

---
+++ StacksQueues/isSorted.java
@@ -0,0 +1,33 @@
+public static boolean isSorted(Stack<Integer> s) {
+    boolean sorted = true;
+    if (s.size() < 2) {
+        sorted = true;
+    } else {
+        Queue<Integer> q = new LinkedList<Integer>();
+        int current;
+        while (!s.isEmpty()) {
+            int prev = s.pop();
+            if (!s.isEmpty()) {
+                current = s.pop();
+                s.push(current);
+                if (prev > current) {
+                    sorted = false;
+                }
+            }
+            q.add(prev);
+        }
+        
+        while (!q.isEmpty()) {
+            s.push(q.remove());
+        }
+        
+        while (!s.isEmpty()) {
+            q.add(s.pop());
+        }
+        
+        while (!q.isEmpty()) {
+            s.push(q.remove());
+        }
+    }
+    return sorted;
+}
