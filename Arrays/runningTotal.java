d635b3e3dde6ac78fcc3bd10ee9cd4838ee85e1b
yulongtan <swifties@uw.edu>
2016-02-10 10:42:25 -0800

    wrote a method that gets running total of a list into a new list

---
+++ Arrays/runningTotal.java
@@ -0,0 +1,10 @@
+public ArrayIntList runningTotal() {
+    ArrayIntList list2 = new ArrayIntList();
+    int sum = 0;
+    for (int i = 0; i < this.size; i++) {
+        sum += this.elementData[i];
+        list2.elementData[i] = sum;
+        list2.size++;
+    }
+    return list2;
+}
