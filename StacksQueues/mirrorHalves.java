d93e7cf1348d6a257b6c69cb4c42d716de97cfbc
Yulong Tan <swifties@uw.edu>
2016-02-15 16:09:15 -0800

    optimized solution

--- StacksQueues/mirrorHalves.java
+++ StacksQueues/mirrorHalves.java
@@ -21,3 +21,24 @@ public static void mirrorHalves(Queue<Integer> q) {
         q.enqueue(s.pop());
     }
 }
+
+/* 2.15.16
+Cleaner solution
+*/
+public static void mirrorHalves(Queue<Integer> q) {
+    if (q == null || q.size() % 2 == 1) {
+        throw new IllegalArgumentException();
+    }
+    Stack<Integer> s = new Stack<Integer>();
+    int size = q.size();
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < size / 2; j++) {
+            int num = q.remove();
+            q.add(num);
+            s.push(num);
+        }
+        while (!s.isEmpty()) {
+            q.add(s.pop());
+        }
+    }
+}
