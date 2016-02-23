21a0c1830cb5004df240325e5c2b9b5f12f51758
yulongtan <swifties@uw.edu>
2016-02-22 18:13:23 -0800

    shifts all odd positions to back

---
+++ LinkedList/shift.java
@@ -0,0 +1,15 @@
+public void shift() {
+    if (this.front != null && this.front.next != null) {
+        // size > 2, guaranteed
+        ListNode current = this.front;
+        ListNode current2 = this.front.next;
+        ListNode current2front = current2;
+        while (current != null && current.next != null && current.next.next != null) {
+            current.next = current2.next;
+            current2.next = current2.next.next;
+            current = current.next;
+            current2 = current2.next;
+        }
+        current.next = current2front;
+    }
+}
