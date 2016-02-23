25ed992ddd5681b91c9cf214be2cbca6b2f124cb
yulongtan <swifties@uw.edu>
2016-02-23 15:14:01 -0800

    method that checks if this tree is equal to another tree

---
+++ Trees/equals.java
@@ -0,0 +1,15 @@
+public boolean equals(IntTree t2) {
+    return this.equals(this.overallRoot, t2.overallRoot);
+}
+
+private boolean equals(IntTreeNode root1, IntTreeNode root2) {
+    if (root1 == null && root2 != null || root1 != null && root2 == null) {
+        return false;
+    } else if (root1 == null && root2 == null) {
+        return true;
+    } else if (root1.data != root2.data) {
+        return false;
+    } else {
+        return this.equals(root1.left, root2.left) && this.equals(root1.right, root2.right);
+    }
+}
