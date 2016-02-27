1cc74986baaaf48e968d0b3d7a3fe21aa9c22a1e
yulongtan <swifties@uw.edu>
2016-02-26 18:14:10 -0800

    updated method

--- Trees/limitPathSum.java
+++ Trees/limitPathSum.java
@@ -1,15 +1,16 @@
-public void limitPathSum(int n) {
-	this.overallRoot = this.limitPathSum(this.overallRoot, max, 0);
+public void limitPathSum(int n) {
+	this.overallRoot = this.limitPathSum(this.overallRoot, n, 0);
 }
 
-private IntTreeNode limitPathSum(IntTreeNode root, int max, int n) {
-	if (root != null) {
-		if (n > max) {
-			root = null;
-		} else {
-			root.left = this.limitPathSum(root.left, max, n + root.data);
-			root.right = this.limitPathSum(root.right, max, n + root.data);
+private IntTreeNode limitPathSum(IntTreeNode root, int max, int sum) {
+	if (root != null) {
+        sum += root.data;
+		if (sum > max) {
+			root = null;
+		} else {
+			root.left = this.limitPathSum(root.left, max, sum);
+			root.right = this.limitPathSum(root.right, max, sum);
 		}
 	}
-	return root;
+	return root;
 }
