1f22aaac5ec18ae169bd40600bf9bb2586953e5f
yulongtan <swifties@uw.edu>
2016-03-02 10:31:32 -0800

    removes all occurrences of n from list

---
+++ LinkedList/removeAll.java
@@ -0,0 +1,15 @@
+public void removeAll(int n) {
+    if (this.front != null) {
+        ListNode current = this.front;
+        while (current != null && current.next != null) {
+            if (current.next.data == n) {
+                current.next = current.next.next;        
+            } else {
+                current = current.next;
+            }
+        }
+        if (this.front.data == n) {
+            this.front = this.front.next;
+        }
+    }
+}
