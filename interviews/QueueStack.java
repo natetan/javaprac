aacade4bdcead21798c880c17aa86f2fece6cac6
yulongtan <swifties@uw.edu>
2016-03-04 11:13:04 -0800

    added comments

--- interviews/QueueStack.java
+++ interviews/QueueStack.java
@@ -3,6 +3,8 @@
  *  why did we even need 2 queues? 1 worked fine	
  *
  *	Integer will be used for the data
+ *	
+ *	The back of the queue will be considered the TOP of the stack.
  */
 
 public class QueueStack {
@@ -10,17 +12,21 @@ public class QueueStack {
 	private Queue<Integer> backup;
 	private int count;
 
+	// Constructs a new QueueStack
 	public QueueStack() {
 		this.storage = new LinkedList<Integer>();
 		this.backup = new LinkedList<Integer>();
 		this.count = 0;
 	}
 
+	// Stores the given int into the Stack
 	public void push(int n) {
 		this.storage.add(n);
 		this.count++;
 	}
 
+	// Removes the element from the TOP of the stack (back of the queue)
+	// and returns it
 	public int pop() {
 		this.shift();	
 		int num = this.storage.remove();
@@ -28,6 +34,8 @@ public class QueueStack {
 		return num;
 	}
 
+	// Returns the int at the top of the stack without changing the order
+	// of the elements
 	public int peek() {
 		this.shift();
 		int num = this.storage.remove();
@@ -35,14 +43,18 @@ public class QueueStack {
 		return num;
 	}
 
+	// Returns the stack's size
 	public int size() {
 		return this.count;
 	}
 
+	// Returns true if the stack is empty and false otherwise
 	public boolean isEmpty() {
 		return this.size() == 0;
 	}
 
+	// Rotates the queue so the last element is at the front.
+	// Throws a NoSuchElementException if it is empty
 	private void shift() {
 		if (this.isEmpty()) {
 			throw new NoSuchElementException();
