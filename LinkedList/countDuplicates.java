c59e232e2e39dc4c08d4a401d5661ebb40fe5f5a
Yulong Tan <swifties@uw.edu>
2016-03-04 22:26:11 -0800

    merge

---
+++ LinkedList/countDuplicates.java
@@ -0,0 +1,15 @@
+public int countDuplicates() {
+    int count = 0;
+    if (this.front == null || this.front.next == null) {
+        return 0;
+    } else { // size > 1
+        ListNode current = this.front;
+        while (current.next != null) {
+            if (current.next.data == current.data) {
+                count++;
+            }
+            current = current.next;
+        }
+    }
+    return count;
+}
