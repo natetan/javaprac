f2ddf70237fa51f3c3621308669ea27a850f3b27
yulongtan <swifties@uw.edu>
2016-03-07 14:01:20 -0800

    fixed missing parameter

--- SetsMaps/acronyms.java
+++ SetsMaps/acronyms.java
@@ -2,7 +2,7 @@ public Map<String, Set<List<String>> acronyms(Set<List<String>> lists) {
 	Map<String, Set<List<String>> map = new TreeMap<String, Set<List<String>>();
 	for (List<String> list : lists) {
 		String acronym = this.acronymFor(list);
-		if (!map.containsKey()) {
+		if (!map.containsKey(acronym)) {
 			map.put(acronym, new Set<List<String>>());
 		}
 		map.get(acronym).add(list);
