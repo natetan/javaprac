15d642f1e1793f6ed0eae64c7f475f89c0414bf7
yulongtan <swifties@uw.edu>
2016-03-07 13:59:24 -0800

    points acronyms to their respective lists

---
+++ SetsMaps/acronyms.java
@@ -0,0 +1,11 @@
+public Map<String, Set<List<String>> acronyms(Set<List<String>> lists) {
+	Map<String, Set<List<String>> map = new TreeMap<String, Set<List<String>>();
+	for (List<String> list : lists) {
+		String acronym = this.acronymFor(list);
+		if (!map.containsKey()) {
+			map.put(acronym, new Set<List<String>>());
+		}
+		map.get(acronym).add(list);
+	}
+	return map;
+}
