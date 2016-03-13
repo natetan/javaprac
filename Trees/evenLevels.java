a0d08d30303637abbe709c4a3d6411a3176150fc
yulongtan <swifties@uw.edu>
2016-03-12 21:34:59 -0800

    Makes sure all branches end on even levels

---
+++ Trees/evenLevels.java
@@ -0,0 +1,15 @@
+public void evenLevels() {
+    this.overallRoot = this.evenLevels(this.overallRoot, 1);
+}
+
+private IntTreeNode evenLevels(IntTreeNode root, int level) {
+    if (root != null) {
+        if (root.left == null && root.right == null && level % 2 == 1) {
+            root = null;
+        } else {
+            root.left = this.evenLevels(root.left, level + 1);
+            root.right = this.evenLevels(root.right, level + 1);
+        }
+    }
+    return root;
+}
