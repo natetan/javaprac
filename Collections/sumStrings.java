9e89733a96d1b57a2b1a330cef87fa57cb325e9b
yulongtan <swifties@uw.edu>
2016-03-13 20:15:29 -0700

    gets sums of strings

---
+++ Collections/sumStrings.java
@@ -0,0 +1,12 @@
+public static Map<Point, Integer> sumStrings(Map<String, Point> map) {
+	Map<Point, Integer> newMap = new HashMap<Point, Integer>();
+	for (String s : map.keySet()) {
+		if (!newMap.containsKey(map.get(s))) {
+			newMap.put(map.get(s), s.length());
+		} else {
+			newMap.get(map.get(s)) += s.length();
+		}
+	}
+	return newMap;
+}
+
