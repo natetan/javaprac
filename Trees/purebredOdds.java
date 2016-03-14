ff14a103d94ed34bced0e151b142463090da877c
yulongtan <swifties@uw.edu>
2016-03-13 20:17:40 -0700

    counts the purebred (all parents are odds) odds in the tree

---
+++ Trees/purebredOdds.java
@@ -0,0 +1,13 @@
+public int purebredOdds() {
+	return this.purebredOdds(this.overallROot);
+}
+
+private int prebredOdds(IntTreeNode root) {
+	if (root == null) {
+		return 0;
+	} else if (root.data % 2 == 0) {
+		return 0;
+	} else {
+		return 1 + this.purebredOdds(root.left) + this.purebredOdds(root.right);
+	}
+}
