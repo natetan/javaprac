adbdaad7a4554ab5e883b9348f7f0cec27455105
Yulong Tan <swifties@uw.edu>
2016-03-14 23:53:38 -0700

    fixed bugs

--- Trees/sumLeaves.java
+++ Trees/sumLeaves.java
@@ -6,8 +6,8 @@ private int sumLeaves(IntTreeNode root, int sum) {
     if (root == null) {
         return 0;
     } else if (root.left == null && root.right == null) {
-        sum += root.data;
+        return sum += root.data;
     } else {
-        return this.sumLeaves(root.left) + this.sumLeaves(root.right);
+        return this.sumLeaves(root.left, sum) + this.sumLeaves(root.right, sum);
     }
 }
