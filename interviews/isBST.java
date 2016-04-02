e68416e67c13bc7f27bea80d7bcc01c6d8fb728e
yulongtan <swifties@uw.edu>
2016-04-01 19:58:41 -0700

    checks whether a tree is a BST or not

---
+++ interviews/isBST.java
@@ -0,0 +1,13 @@
+public boolean isBST() {
+	return this.isBST(this.overallRoot, Integer.MIN_VALUE, Integer.MAX_VALUE);
+}
+
+private boolean iSBST(IntTreeNode root, int min, int max) {
+	if (root == null) {
+		return true;
+	} else if (root.data <= min || root.data >= max) {
+		return false;
+	} else {
+		return this.isBST(root.left, min, root.data) && this.isBST(root.right, max, root.data);
+	}
+}
