6284a51601947f6386b2520cd88b42d72c2686a7
Yulong Tan <yulongtan67@gmail.com>
2016-12-17 13:00:47 -0800

    toString

--- Comparable/BankAccount.java
+++ Comparable/BankAccount.java
@@ -1,10 +1,12 @@
 public class BankAccount {
 
+	private String name;
 	private String id;
 	private double balance;
 	private int transactions;
 
-	public BankAccount(String id) {
+	public BankAccount(String name, String id) {
+		this.name = name;
 		this.id = id;
 		this.balance = 0;
 		this.transactions = 0;
@@ -34,5 +36,33 @@ public class BankAccount {
 			this.balance -= amount;
 			this.transactions++;
 		}
+	}
+
+	public boolean transactionFee(double fee) {
+		if (this.balance == 0) {
+			return false;
+		} else {
+			double totalFee = 0;
+			for (int i = 0; i < this.transactions; i++) {
+				totalFee += fee * i;
+			}
+			if (this.balance >= totalFee) {
+				this.balance -= totalFee;
+				return true;
+			} else {
+				this.balance = 0;
+				return false;
+			}
+		}
+	}
+
+	public String toString() {
+		String negative = "";
+		if (this.balance < 0) {
+			negative = "-";
+			this.balance *= -1;
+		}
+		this.balance = Math.round(this.balance * 100.00) / 100.00;
+		return this.name + ", " + negative + "$" + this.balance;
 	}
 }
