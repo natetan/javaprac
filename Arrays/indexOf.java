aa4034b053a335bf84ca849b5401487564593adf
Yulong Tan <yulongtan67@gmail.com>
2016-12-23 17:06:17 -0800

    refactor code to not use break

--- Arrays/indexOf.java
+++ Arrays/indexOf.java
@@ -1,11 +1,8 @@
 public static int indexOf(int[] list, int num) {
-    int index = -1;
-    for (int i = 0; i < list.length; i++) {
-        if (list[i] == num) {
-            index = i;
-            break;
-            
-        }
-    }
-    return index;
+	for (int i = 0; i < list.length; i++) {
+		if (list[i] == num) {
+			return i;
+		}
+	}
+	return -1;
 }
